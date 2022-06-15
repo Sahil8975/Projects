92a8ac01fbbe83fbf608256fd6902dda0bb79b37
Sahil <sahilmujawar8975@gmail.com>
2022-06-15 17:50:50 +0530

    updates

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -12,7 +12,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { emailValidator, passwordValidator } from '../../components/regexValidator';
+import { emailValidator,passwordValidator } from '../../Header/regexValidator';
 import {useHistory} from "react-router-dom"
 
 function Copyright(props) {
